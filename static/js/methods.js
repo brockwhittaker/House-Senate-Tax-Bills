db73ff7b3f5b115ae64fb8dbdfc5cac380237819
brockwhittaker <bjwhitta@asu.edu>
2017-12-06 00:15:08 -0800

    Add dependents and the front end for filing status.

--- static/js/methods.js
+++ static/js/methods.js
@@ -19,20 +19,27 @@ function debounce(func, wait, immediate) {
 
 const methods = {
     onIncomeUpdate: (() => {
-        let lastState = null;
+        let last = {
+            USState: null,
+            numDependents: null,
+            filingStatus: null,
+        };
 
         return function (e) {
-            const { USState, incomeRange } = this.$refs;
-            const state = USState.value;
+            const { incomeRange, USState, numDependents, filingStatus } = this.$refs;
 
-            store.state = state;
+            store.state = USState.value;
             store.income = taxes.convertLinearRangeToIncome(incomeRange.value);
-            store.tax.state = taxes.calculateStateTaxBurden(store.income, false, TAX_RATES[2017].state[state]);
+            store.tax.state = taxes.calculateStateTaxBurden(
+                store.income,
+                false,
+                TAX_RATES[2017].state[USState.value]
+            );
 
             ["current", "senate", "house"].forEach(bill => {
                 store.tax[bill] = taxes.calculateFederalTaxBurden[bill]({
                     income: store.income,
-                    numExemptions: 1,
+                    numExemptions: 1 + (+numDependents.value),
                     SALT: store.tax.state,
                 });
             });
@@ -41,18 +48,22 @@ const methods = {
                 store.taxDiff[bill] = store.tax[bill] / store.tax.current - 1;
             });
 
-            if (state !== lastState) {
+            if (USState.value !== last.USState || numDependents.value !== last.numDependents) {
                 methods.renderChart();
-                lastState = state;
+
+                Object.assign(last, {
+                    USState: USState.value,
+                    numDependents: numDependents.value,
+                    filingStatus: filingStatus.value
+                });
             }
-
-
-            //debounce(methods.renderChart, 500, true);
         }
     })(),
 
     renderChart: () => {
         const state = vue.$refs.USState.value;
+        const numDependents = vue.$refs.numDependents.value;
+        const numExemptions = 1 + (+numDependents);
 
         const arr = [];
         for (let x = 0; x < 100; x++) {
@@ -71,7 +82,7 @@ const methods = {
                 data: arr.map(income => {
                     return taxes.calculateFederalTaxBurden[schedule.bill]({
                         income: income,
-                        numExemptions: 1,
+                        numExemptions,
                         SALT: taxes.calculateStateTaxBurden(income, false, TAX_RATES[2017].state[state]),
                     }) / income * 100;
                 }),
@@ -86,12 +97,12 @@ const methods = {
                 data: arr.map(income => {
                     const percentTaxOnBill = taxes.calculateFederalTaxBurden[schedule.bill]({
                         income: income,
-                        numExemptions: 1,
+                        numExemptions,
                         SALT: taxes.calculateStateTaxBurden(income, false, TAX_RATES[2017].state[state]),
                     }) / income * 100;
                     const percentTaxOnCurrent = taxes.calculateFederalTaxBurden.current({
                         income: income,
-                        numExemptions: 1,
+                        numExemptions,
                         SALT: taxes.calculateStateTaxBurden(income, false, TAX_RATES[2017].state[state]),
                     }) / income * 100;
 
